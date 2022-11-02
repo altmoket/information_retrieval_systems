3962f4a8b0f2a581d23c06ec5d3b45eb04ab67aa
altmoket <copy90471@gmail.com>
2022-11-02 19:36:19 -0400

    Method to obtain all words frequency in documents

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from src.utils import get_all_file_names, get_words_frequency
+from src.utils import get_all_file_names, get_all_words_frequency, get_words_frequency
 
 
 text = "Hello World friend, World"
 
-a = get_all_file_names('documents')
-b = get_words_frequency(text)
+# a = get_all_file_names('docs')
+# b = get_words_frequency(text)
+c = get_all_words_frequency('docs')
+for key, value in c.items():
+    print(key, value)
 
-print(a)
-print(b)
+
+# print(a)
+# print(b)
+# print(c)
--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,18 @@ def get_words_frequency(text:str):
     for word in re.findall('\w+', text):
         counts[word] += 1
     return counts
+
+def get_all_words_frequency(path:str):
+    filenames = get_all_file_names(path)
+    results = defaultdict(int)
+    for filename in filenames:
+        with open(f"{path}/{filename}") as f:
+            counts = get_words_frequency(f.read())
+            for key,value in counts.items():
+                if results[key] is not None:
+                    results[key] += value
+                else:
+                    results[key] = value
+    return results
+
+
