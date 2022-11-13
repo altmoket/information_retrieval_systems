66335480c80e1f6c9d68cf0b050e40cdf52d2d81
altmoket <copy90471@gmail.com>
2022-11-12 22:26:48 -0500

    Concluyendo metodos de tf and idf con todos sus submetodos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import ir_datasets
+from ir_datasets.util import math
 import stop_words
 # print(dataSet.docs_count())
 # print(dataSet.queries_count())
@@ -25,6 +26,54 @@ def removeStopWordsInTokens(tokens):
         except:
             newTokens.append(token)
     return newTokens
+
+def frequencyTermInDoc(term, doc):
+    frequency = 0
+    INCREMENT = 1
+    for word in doc:
+        if term == word:
+            frequency += INCREMENT
+    return frequency
+
+def maxFrequencyInDoc(doc):
+    maxFrequency = 0
+    for term in doc:
+        frequency = frequencyTermInDoc(term, doc)
+        if frequency > maxFrequency:
+            maxFrequency = frequency
+    return maxFrequency
+
+def tfTermInDoc(term, doc):
+    frequency = frequencyTermInDoc(term, doc)
+    maxFrequency = maxFrequencyInDoc(doc)
+    return frequency/maxFrequency
+
+def existTermInDoc(term, doc):
+    EXIST = True
+    for word in doc:
+        if word == term:
+            return EXIST
+    return not EXIST
+
+def numberOfDocumentsWhichContainsTerm(docs, term):
+    numberOfDocuments = 0
+    INCREMENT = 1
+    for doc in docs:
+        existTerm = existTermInDoc(term, doc)
+        if existTerm:
+            numberOfDocuments += INCREMENT
+    return numberOfDocuments
+
+def idfTermInDocs(term, docs):
+    numberOfDocuments = len(docs)
+    documentsWichContainsTerm = numberOfDocumentsWhichContainsTerm(docs, term)
+    idf = math.log(numberOfDocuments/documentsWichContainsTerm)
+    return idf
+    
+
+
+
+
 
 def main():
     CORPUS = "cranfield"
