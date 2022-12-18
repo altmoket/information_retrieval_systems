fee148959868fc126ecc19772bf984ad10049a47
altmoket <copy90471@gmail.com>
2022-12-18 11:40:42 -0500

    feat: Add CollectionNotFound Exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,3 +3,7 @@
 class WrongDocCollectionNameException(Exception):
     def __str__(self) -> str:
         return "Wrong Doc Collection Name Exception"
+    
+class CollectionNotFound(Exception):
+    def __str__(self) -> str:
+        return "Collection not found"
