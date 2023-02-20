f85f1e8227fcefbd00f498948f03ba514a1adfb2
altmoket <copy90471@gmail.com>
2023-02-20 00:06:09 -0500

    feat: Update corpus parser interface

---
+++ sri/searchengine/parser/interfaces.py
@@ -0,0 +1,27 @@
+
+
+
+class CorpusParser:
+    def __init__(self, config) -> None:
+        self.docs_path = config.docs
+        self.qry_path = config.qry
+        self.rel_path = config.rel
+    
+    def get_data(self):
+        NotImplementedError()
+
+    
+    def get_txt_data(self):
+        NotImplementedError()
+
+    
+    def get_qry_data(self):
+        NotImplementedError()
+
+    
+    def get_rel(self):
+        NotImplementedError()
+
+    
+    def get_rel_numpy(self):
+        NotImplementedError()
