b4116d83bc7d91e888ffc955526c44521e8192ce
altmoket <copy90471@gmail.com>
2023-02-20 00:37:00 -0500

    feat: Add flask to main container

--- sri/containers.py
+++ sri/containers.py
@@ -1,5 +1,6 @@
 from dependency_injector import containers, providers
 from .searchengine.containers import SearchEngineContainer
+from .navigator.containers import FlaskContainer
 
 
 class SriContainer(containers.DeclarativeContainer):
@@ -9,6 +10,12 @@ class SriContainer(containers.DeclarativeContainer):
     search_engine_package = providers.Singleton(
         SearchEngineContainer,
         config = config
+    )
+    
+    flask_container = providers.Singleton(
+        FlaskContainer,
+        config = config.flask,
+        search_engine = search_engine_package.search_engine_service
     )
     
     
