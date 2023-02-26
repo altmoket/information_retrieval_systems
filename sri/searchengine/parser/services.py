2afb3918fc8a31f526241886c9dc863e877125d9
altmoket <copy90471@gmail.com>
2023-02-26 15:36:21 -0500

    Update parser services

--- sri/searchengine/parser/services.py
+++ sri/searchengine/parser/services.py
@@ -6,22 +6,22 @@ class CorpusService:
         self.cranfield_parser = cranfield_parser
         self.cisi_parser = cisi_parser
         self.medline_parser = medline_parser
-        self.activate_collection()
+        self.activate_corpus()
         
-    def activate_collection(self, collection:str = "cranfield"):
-        if collection == "cranfield":
+    def activate_corpus(self, corpus:str = "cranfield"):
+        if corpus == "cranfield":
             self._parser = self.cranfield_parser
-            self.collection_name = "cranfield"
-        elif collection == "medline":
+            self.corpus_name = "cranfield"
+        elif corpus == "medline":
             self._parser = self.medline_parser
-            self.collection_name = "medline"
-        elif collection == "cisi":
+            self.corpus_name = "medline"
+        elif corpus == "cisi":
             self._parser = self.cisi_parser
-            self.collection_name = "cisi"
+            self.corpus_name = "cisi"
 
     def get_documents(self):
         try:
-            info = self.get_file(f'db/preprocess/{self.collection_name}_docs.json')
+            info = self.get_file(f'db/preprocess/{self.corpus_name}_docs.json')
             return info
         except:
             txt_data = self._parser.get_txt_data()
@@ -30,7 +30,7 @@ class CorpusService:
 
     def get_queries(self):
         try:
-            info = self.get_file(f'db/preprocess/{self.collection_name}_qry.json')
+            info = self.get_file(f'db/preprocess/{self.corpus_name}_qry.json')
             return info
         except:
             qry_data = self._parser.get_qry_data()
@@ -39,7 +39,7 @@ class CorpusService:
 
     def get_rel(self):
         try:
-            info = self.get_file(f'db/preprocess/{self.collection_name}_rel.json')
+            info = self.get_file(f'db/preprocess/{self.corpus_name}_rel.json')
             return info
         except:
             rel = self._parser.get_rel()
@@ -48,7 +48,7 @@ class CorpusService:
     
     def save_in_json(self, data, filename):
         self.create_preprocess_folder()
-        with open(f'db/preprocess/{self.collection_name}_{filename}.json','w') as f:
+        with open(f'db/preprocess/{self.corpus_name}_{filename}.json','w') as f:
             result = json.dump(data, f, indent=4)
             return result
             
